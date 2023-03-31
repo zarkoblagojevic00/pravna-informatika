98ce57e9377cb469f2dbd868908a636bf5bf6d50
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2023-03-31 16:33:09 +0200

    backend initialization

---
+++ backend/src/main/java/pravnainformatika/PravnaInformatikaApplication.java
@@ -0,0 +1,13 @@
+package pravnainformatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PravnaInformatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PravnaInformatikaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pravnainformatika/PravnaInformatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.pravnainformatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravnaInformatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
